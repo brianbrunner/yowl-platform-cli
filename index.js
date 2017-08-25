6ece8459e4734c75bb224b73ea51264c6141f57c
Brian Brunner <brian.brunner@gmail.com>
2017-08-25 15:58:41 -0700

    Proper support for actions capability

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var argv = require('yargs').argv,
+    unfluff = require('unfluff'),
+    request = require('request'),
     local = argv.local,
     auth = argv.auth,
     sessionId = argv.session || "local";
@@ -22,6 +24,8 @@ var platform = module.exports = function(bot) {
 
 };
 
+var current_actions;
+
 platform.id = "local";
 platform.capabilities = ["say", "actions"];
 platform.send = function(context, event, response, cb) {
@@ -30,8 +34,22 @@ platform.send = function(context, event, response, cb) {
   } else {
     console.log(bot_name + ' > ' + response.message);
     if (response.actions) {
-      var actionsString = '[' + response.actions.join(',') + ']';
-      console.log(bot_name + ' > ' + actionsString);
+      current_actions = response.actions;
+      var action_strings = response.actions.map(function(action) {
+        if (typeof action == "string") {
+          return action;
+        } else {
+          var action_string = action.title;
+          if (action.url) {
+            action_string += ' (' + action.url + ')'
+          }
+          return action.title;
+        }
+      });
+      console.log(bot_name + ' > Choose an action by entering its number:');
+      action_strings.forEach(function(action_string, index) {
+        console.log(''+index+' > ' + action_string);
+      });
     }
   }
   cb(null, context, event, response);
@@ -43,7 +61,24 @@ function listener(bot) {
   return function(buffer) {
     var line = buffer.toString();
     var truncatedLine = line.slice(0,line.length-1);
-    bot(platform, { sessionId: sessionId }, { type: 'message', message: truncatedLine }, oncomplete);
+    if (current_actions && !isNaN(parseInt(truncatedLine))) {
+      var action = current_actions[parseInt(truncatedLine)];
+      current_actions = null;
+      if (typeof action === "string") {
+        bot(platform, { sessionId: sessionId }, { type: 'action', message: action, action: action }, oncomplete);
+      } else if (action.url) {
+        request(action.url, function (error, response, body) {
+          var article = unfluff(body);
+          console.log(bot_name + ' > Processed Body For ' + article.title);
+          console.log('\n'+article.text+'\n');
+          oncomplete(null, null, null, function() {});
+        });
+      } else {
+        bot(platform, { sessionId: sessionId }, { type: 'action', message: action.payload, action: action.payload }, oncomplete);
+      }
+    } else {
+      bot(platform, { sessionId: sessionId }, { type: 'message', message: truncatedLine }, oncomplete);
+    }
   };
 }
 
